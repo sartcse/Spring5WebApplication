c3fea79fd61625735ba59a4f4c14a7f987ab2953
saravanakumar.gs <saravanakumar.gs@accenture.com>
2018-02-01 11:28:26 -0600

    Adding Spring Boot Files

---
+++ src/main/java/com/sartcse/Spring5WebApplication/Spring5WebApplication.java
@@ -0,0 +1,12 @@
+package com.sartcse.Spring5WebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebApplication.class, args);
+	}
+}
